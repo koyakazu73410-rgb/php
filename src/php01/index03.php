4673461e7e9a8759e844d421e7afda4855841f92
kazuki <koyakazu73410@gmail.com>
2026-02-14 00:47:14 +0900

    リモートリポジトリの変更

---
+++ src/php01/index03.php
@@ -0,0 +1,8 @@
+<?php
+$name1 = "Tanaka" . "Jiro";
+$name2 = "Yamada" ."Saburo";
+
+echo $name1;
+echo "<br />";
+echo $name2;
+echo "<br />";
---
+++ src/php01/index05.php
@@ -0,0 +1,7 @@
+<?php
+$people = "Saburo";
+
+switch ($people) {
+    case "Saburo":
+        echo "三郎です";
+}
